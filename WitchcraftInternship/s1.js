5cd5a4050e873d3eec9a3fbd2a49485a38dfc6d3
nzhiqi <107100845+nzhiqi@users.noreply.github.com>
2022-12-06 14:49:19 +0800

    Add files via upload

---
+++ WitchcraftInternship/s1.js
@@ -0,0 +1,36 @@
+class s1 extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: "s1" });
+    }
+
+    preload() {
+      this.load.image('s1', 'assets/s1.png')
+  
+  }
+  
+  create () {
+      this.S1 = this.add.image(0, 0, 's1').setOrigin(0, 0).setScale(0.09);
+     
+      console.log("*** world scene");
+      let map = this.make.tilemap({ key: "world1" });
+  
+      var spaceDown = this.input.keyboard.addKey('SPACE');
+      var enterDown = this.input.keyboard.addKey('enter');
+      
+      spaceDown.on('down', function(){
+      this.scene.stop("s1");
+      this.scene.start("s2");
+      }, this );
+  
+      enterDown.on('down', function(){
+        console.log("skip tutorial");
+        this.player = {};
+        this.player.x= 489;
+        this.player.y=378;
+        this.scene.start("world", {player: this.player});
+        }, this );
+  }
+    
+  }
---
+++ WitchcraftInternship/winningScene.js
@@ -0,0 +1,39 @@
+class winningScene extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: "winningScene" });
+    }
+  
+    preload() {
+      this.load.image('winningScene', 'assets/win.jpg')
+  
+  }
+  
+  create () {
+      this.winningScene = this.add.image(-30, -8, 'winningScene').setOrigin(0, 0).setScale(0.095);
+      // window.music.stop();
+      this.winSnd = this.sound.add("win");
+    
+  
+      var spaceDown = this.input.keyboard.addKey('SPACE');
+    //   var enterDown = this.input.keyboard.addKey('enter');
+      
+      spaceDown.on('down', function(){
+      this.scene.stop("winningScene");
+      this.scene.start("level1");
+      }, this );
+
+      this.winSnd.play();
+  
+    //   enterDown.on('down', function(){
+    //     console.log("skip tutorial");
+    //     this.player = {};
+    //     this.player.x= 451;
+    //     this.player.y=255.8;
+    //     this.scene.start("level4", {player: this.player});
+    //     }, this );
+  
+  }
+    
+  }
